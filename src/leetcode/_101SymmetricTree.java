70068a0a5800bc665c05fc471d844e41ea4cdbc5
Nagabharan N <nnagabharan@gmail.com>
2016-09-27 19:22:53 -0500

    leetcode

---
+++ src/leetcode/_101SymmetricTree.java
@@ -0,0 +1,28 @@
+package leetcode;
+
+/**
+ * Created by nagabharan on 27-Sep-16.
+ */
+public class _101SymmetricTree {
+    static boolean isSymmetric(TreeNode root) {
+        if (root == null)
+            return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    static boolean isSymmetric(TreeNode l, TreeNode r) {
+        if (l == null && r == null)
+            return true;
+        if (l == null || r == null)
+            return false;
+        if (l.val != r.val)
+            return false;
+        if (!isSymmetric(l.left, r.right))
+            return false;
+        return isSymmetric(l.right, r.left);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/leetcode/_14LongestCommonPrefix.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+/**
+ * Created by nagabharan on 27-Sep-16.
+ */
+public class _14LongestCommonPrefix {
+    static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+        if (strs.length == 1)
+            return strs[0];
+        int minlenstr = strs.length + 1;
+        for (String str : strs) {
+            if (minlenstr > str.length())
+                minlenstr = str.length();
+        }
+        for (int i = 0; i < minlenstr; i++) {
+            for (int j = 0; j < strs.length - 1; j++) {
+                String s1 = strs[j];
+                String s2 = strs[j + 1];
+                if (s1.charAt(i) != s2.charAt(i))
+                    return s1.substring(0, i);
+            }
+        }
+        return strs[0].substring(0, minlenstr);
+    }
+
+    public static void main(String[] args) {
+        String[] strs = {"Hello", "He"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
